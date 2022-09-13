0c071dac49eea5d9ac3a618f658b8ef0a98e157f
mdasif8393 <md.asif.8393@gmail.com>
2022-09-13 22:34:59 +0600

    add delete and upddate

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const ObjectId = require("mongodb").ObjectId;
+
 //use middleware
 app.use(cors());
 app.use(express.json());
@@ -30,6 +32,7 @@ async function run() {
     const database = client.db("foodExpress");
     const userCollection = database.collection("user");
     // create a document to insert
+
     //create
     app.post("/user", async (req, res) => {
       const user = req.body;
@@ -37,12 +40,44 @@ async function run() {
       res.json(result);
     });
 
-    //Read
+    //Read all user
     app.get("/user", async (req, res) => {
       const query = {};
       const cursor = userCollection.find(query);
       const users = await cursor.toArray();
       res.json(users);
+    });
+
+    //get a user
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+    //Delete a user
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //update a user
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: user.name,
+          email: user.email,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
     });
   } finally {
     //await client.close();
